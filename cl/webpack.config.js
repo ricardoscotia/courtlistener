90a30f0aeeae9e0620a696d5045595c8626b937f
Michael Lissner <mlissner@michaeljaylissner.com>
2020-09-16 13:06:33 -0700

    feat(react): Add ts and webpack configs for static dirs

--- cl/webpack.config.js
+++ cl/webpack.config.js
@@ -15,9 +15,10 @@ module.exports = {
   context: __dirname,
   mode: isDevelopment ? 'development' : 'production',
 
-  entry: [
-    './assets/react/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
-  ],
+  entry: {
+    main: './assets/react/index',
+    modal: './favorites/static/react/tagEditModal',
+  },
   output: isDevelopment
     ? // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
       { ...baseOutput, publicPath: 'http://localhost:3000/assets/bundles/' }
@@ -40,7 +41,7 @@ module.exports = {
     rules: [
       {
         test: /\.(jsx|ts|tsx)?$/,
-        include: path.join(__dirname, 'assets', 'react'),
+        include: [path.join(__dirname, 'assets', 'react'), new RegExp(path.join(__dirname, '.*', 'static', 'react'))],
         loader: 'babel-loader',
       },
     ],
@@ -62,3 +63,6 @@ module.exports = {
   },
   devtool: 'cheap-module-source-map',
 };
+
+/* Print out / debug settings */
+// console.dir(module.exports.module.rules);
